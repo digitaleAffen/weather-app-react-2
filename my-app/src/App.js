db1c55981455a23e3b5c839988a7b29c1fe6d2cb
digitaleAffen <digitaleaffen@gmail.com>
2023-01-20 20:53:02 +0100

    update folders name

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World</h1>
+        <Weather city="bogota" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(handleResponse);
+  console.log(apiUrl);
+}
